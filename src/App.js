32cce524e0fc081cb0e98813f77cbbe7904b26e8
Gabriel Toshinori Nakano <gtnakano@daeesp.gov>
2019-10-07 10:41:23 -0300

    Initial Commit - Ready to code with React, router-dom, axios and semantic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return (
+    <div>
+      <Button>Teste</Button>
+    </div>
+  );
+}
+
+export default App;
